e5a94e3fb65ee4a339da666308a641aa6adafdd6
Lenovo <gabinash018@gmail.com>
2024-04-26 09:44:19 +0530

    Initial commit

---
+++ src/main/java/com/airabinash/abinashairline/AbinashairlineApplication.java
@@ -0,0 +1,13 @@
+package com.airabinash.abinashairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbinashairlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbinashairlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airabinash/abinashairline/AbinashairlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airabinash.abinashairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbinashairlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
